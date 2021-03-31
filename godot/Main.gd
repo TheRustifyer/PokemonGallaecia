d8d937185f27337e2d0e4a67d41463044fdab170
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2021-03-31 13:54:11 +0200

    A

---
+++ godot/Main.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var LoginScreen = preload("res://godot/Background.gdns")
+
+func _init():
+	print("Constructed!")
+	var my_rust_instance = LoginScreen.new()
+	var rust_method = my_rust_instance.set_label_text("LearnRustWithGodot/VBoxContainer/Label", "Text to change")
+	print(rust_method) # This method returns null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('Hi there from main!')
+	print('***************')
