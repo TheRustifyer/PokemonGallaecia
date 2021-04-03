bfcd4ee57b4a6a10b4f038cade3bddd178e1b04e
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2021-04-03 22:32:13 +0200

    Autocompiler by Python

--- cfg.py
+++ cfg.py
@@ -1,12 +1,79 @@
 import os
+import sys
+import platform
 import subprocess
+import re
 
-user_path = os.path.expanduser("~")
+# Getting the base details
 current_path = os.path.abspath((os.getcwd()))
+OS = platform.system()
 
-# for root, dirs, files in os.walk(current_path):
-#     print(root, dirs, files)
-print(os.listdir(current_path))
-# print(subprocess.Popen(['copy']))
-print(current_path)
-print(user_path)
+# The base dirs
+godot_dir = current_path + '\godot'
+rust_dir = current_path + '\\rust'
+
+# Setting concrete folders
+rust_compiling_folder = rust_dir + '\\target\debug'
+rust_project_name = 'learn_programming_with_godot'
+
+# Setting actions based on what OS is currently running
+if OS == 'Windows':
+    desired_extension = '.dll'
+    command = 'copy'
+    silently_override_file_switch = '/Y'
+elif OS == 'Linux':
+    desired_extension = '.dylib'
+    command = 'cp'
+else:
+    pass # Not interested in working with Mac nowadays...
+
+# This will give us back a list with all files inside
+rust_files = os.listdir(rust_compiling_folder)
+
+# Finding out target dynamic library
+candidates = [file for file in rust_files if re.search(rf'\{desired_extension}$', file)]
+library = candidates[0]
+
+# The final path of the desired dynamic lib
+rust_dll_path = rust_compiling_folder + '\\' + library
+
+# Setting our base commands, flags and paths
+subprocess_args = [command, rust_dll_path, godot_dir]
+
+def replace_rust_dll():
+    if OS == 'Windows':
+        from rust.autocompiler import compile_rust
+
+        # First gonna autocompile the Rust .dll
+        compile_rust(rust_compiling_folder)
+
+        subprocess_args.insert(1, silently_override_file_switch)
+        process = subprocess.Popen(
+            subprocess_args, 
+            shell=True, 
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        
+        process_stderr = process.stderr.read(100)
+        # //TODO: Still have to write the code to handle stderr
+        
+        if not process_stderr:
+            process_stdout = process.stdout.read(100)
+            execution_result = process_stdout.decode('UTF-8').strip()
+            print('Coping files, wait...')
+            print('\t' + execution_result)
+
+        '''To avoid use PIPES on stdout and stderr...
+        and in W10, prompt gets waiting if no PIPE de process... so...'''
+        subprocess.call(['exit'], shell=True)
+    else:
+        pass
+    
+if __name__ == '__main__':
+    replace_rust_dll()
+
+
+
+
+
---
+++ rust/autocompiler.py
@@ -0,0 +1,26 @@
+import subprocess
+import os
+
+def compile_rust(path):
+    os.chdir(path)
+    print('Path of __init__.py file ' + os.path.abspath(os.getcwd()))
+    process = subprocess.Popen(
+            'cargo build', 
+            shell=True, 
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    process_stderr = process.stderr.read(100)
+    # //TODO: Still have to write the code to handle stderr
+    
+    if not process_stderr:
+        process_stdout = process.stdout.read(1000)
+        execution_result = process_stdout.decode('UTF-8').strip()
+        print('Coping files, wait...')
+        print('\t' + execution_result)
+
+    subprocess.call(['exit'], shell=True)
+
+    os.chdir(path)
+    print('Path of __init__.py file' + os.path.abspath(os.getcwd()))
