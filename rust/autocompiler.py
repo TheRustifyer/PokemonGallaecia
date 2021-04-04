575f741723309d1753817443e9e65aaaa538ce73
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2021-04-04 22:13:58 +0200

    Solved threading issues while 'cargo run'

--- rust/autocompiler.py
+++ rust/autocompiler.py
@@ -10,6 +10,13 @@ def compile_rust(prev_path, desired_path):
             stdout=subprocess.PIPE,
         )
 
+    '''NOTE: Cargo build command gives his output always on stderr,
+    regardless of whether the program compiled successfully or not, 
+    which is a little bit weird. BTW, we are capturing stdout in order to make the program
+    wait for the complete execution of the process, otherwise Python process will running without
+    wait Rust compilation to ends. (When subprocess with cargo build spawns, it's independent of the
+    Python interpreter thread.'''
+
     process_stdout = process.stdout.read(1000)
     execution_result_out = process_stdout.decode('UTF-8').strip()
     if not process_stdout:
